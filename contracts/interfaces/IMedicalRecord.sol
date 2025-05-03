b79c6f922d53d46767e8be2737db2cad8b505325
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-05-04 00:31:26 +0700

    interface medical record 4-5-2025

---
+++ contracts/interfaces/IMedicalRecord.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IMedicalRecord {
+    function addRecord(string memory ipfsHash) external;
+    function viewRecords(address patient) external view returns (string[] memory, uint[] memory);
+}
