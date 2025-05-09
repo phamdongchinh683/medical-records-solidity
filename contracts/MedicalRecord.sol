5b8a66ba8bce8fde5ac25763abc5d383471a174a
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-05-09 23:45:15 +0700

    update 9-5-2025

--- contracts/MedicalRecord.sol
+++ contracts/MedicalRecord.sol
@@ -8,29 +8,30 @@ import "./interfaces/IMedicalRecord.sol";
 
 contract MedicalRecord is UserRegistry, AccessControl, IMedicalRecord {
     using Structs for Structs.Record;
-    
+
     mapping(address => Structs.Record[]) private records;
-    
+
     function addRecord(string memory ipfsHash) external override {
         require(isPatient[msg.sender], "Not a registered patient");
         records[msg.sender].push(Structs.Record(ipfsHash, block.timestamp));
     }
 
-    function viewRecords(address patient) external view override returns (string[] memory, uint[] memory) {
+    function viewRecords(address patient)
+        external
+        view
+        override
+        returns (string[] memory, uint256[] memory)
+    {
         require(checkAccess(patient, msg.sender), "You do not have access");
-        uint length = records[patient].length;
+        uint256 length = records[patient].length;
         string[] memory hashes = new string[](length);
-        uint[] memory timestamps = new uint[](length);
+        uint256[] memory timestamps = new uint256[](length);
 
-        for (uint i = 0; i < length; i++) {
+        for (uint256 i = 0; i < length; i++) {
             hashes[i] = records[patient][i].ipfsHash;
             timestamps[i] = records[patient][i].timestamp;
         }
 
         return (hashes, timestamps);
     }
-
-
- 
-
 }
